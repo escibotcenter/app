1d084b3bd2277932a6e4fbd4ed73df6a4c68d7af
escibotcenter <sunkungtv85@gmail.com>
2017-06-18 11:10:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,21 @@
 <?php
-echo "I am a bot";
+
+// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
+
+include ('line-bot-api/php/line-bot.php');
+
+$channelSecret = 'Ufe365fb8031e710a9cbbe4b68eeea8eb';
+$access_token  = 'tZZ+yH0la+nWqDqUrW6YDX9Nvm6T//d6IpKZ5JftmLIINbCMigyAxkxJWc8OnR28zpZajioUhnT5lbbjnCYKAbDdcLP8e3ndcOiKEaPVDf1kGL5UBMHJtgoyfSrxvsTW6nr0oaIVoZjWOPwU1piYcwdB04t89/1O/w1cDnyilFU=';
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+$bot->sendMessageNew('[Your userId / User Id]', 'Hello World !!');
+
+if ($bot->isSuccess()) {
+	echo 'Succeeded!';
+	exit();
+}
+
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
